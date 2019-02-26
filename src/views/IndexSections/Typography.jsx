111d6f5584172a9ed2aa9332dc0df081047399f8
Manu <emanuelnazare@yahoo.ro>
2019-02-26 17:08:23 +0200

    - Typography is done

--- src/views/IndexSections/Typography.jsx
+++ src/views/IndexSections/Typography.jsx
@@ -20,37 +20,37 @@ class Typography extends React.Component {
                 <div className="typography-line">
                   <h1>
                     <span>Header 1</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h1>
                 </div>
                 <div className="typography-line">
                   <h2>
                     <span>Header 2</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h2>
                 </div>
                 <div className="typography-line">
                   <h3>
                     <span>Header 3</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h3>
                 </div>
                 <div className="typography-line">
                   <h4>
                     <span>Header 4</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h4>
                 </div>
                 <div className="typography-line">
                   <h5>
                     <span>Header 5</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h5>
                 </div>
                 <div className="typography-line">
                   <h6>
                     <span>Header 6</span>
-                    The Life of BLK• Design System
+                    The Life of BLK• Design System React
                   </h6>
                 </div>
                 <div className="typography-line">
