255155156d6312d8078e84ae9c15d3a9ebb891d5
Manu <emanuelnazare@yahoo.ro>
2019-02-27 13:37:47 +0200

    - Working on Foundation

--- src/documentation.js
+++ src/documentation.js
@@ -7,28 +7,33 @@ import Contents from "views/Documentation/Sections/getting-started/Contents.jsx"
 import BuildTools from "views/Documentation/Sections/getting-started/BuildTools.jsx";
 import Tutorial from "views/Documentation/Sections/getting-started/Tutorial.jsx";
 import Variables from "views/Documentation/Sections/getting-started/Variables.jsx";
+// Foundation
+import Colors from "views/Documentation/Sections/foundation/Colors.jsx";
+import Grid from "views/Documentation/Sections/foundation/Grid.jsx";
+import Typography from "views/Documentation/Sections/foundation/Typography.jsx";
+import Icons from "views/Documentation/Sections/foundation/Icons.jsx";
 // Core components
-import FixedPlugin from "views/Documentation/Sections/FixedPlugin.jsx";
-import Footer from "views/Documentation/Sections/Footer.jsx";
-import Navbar from "views/Documentation/Sections/Navbar.jsx";
-import Sidebar from "views/Documentation/Sections/Sidebar.jsx";
+// import FixedPlugin from "views/Documentation/Sections/FixedPlugin.jsx";
+// import Footer from "views/Documentation/Sections/Footer.jsx";
+// import Navbar from "views/Documentation/Sections/Navbar.jsx";
+// import Sidebar from "views/Documentation/Sections/Sidebar.jsx";
 // Reasctrap components
-import Alerts from "views/Documentation/Sections/Alerts.jsx";
-import Badges from "views/Documentation/Sections/Badges.jsx";
-import Breadcrumbs from "views/Documentation/Sections/Breadcrumbs.jsx";
-import Buttons from "views/Documentation/Sections/Buttons.jsx";
-import Cards from "views/Documentation/Sections/Cards.jsx";
-import Dropdowns from "views/Documentation/Sections/Dropdowns.jsx";
-import Forms from "views/Documentation/Sections/Forms.jsx";
-import Modals from "views/Documentation/Sections/Modals.jsx";
-import Navs from "views/Documentation/Sections/Navs.jsx";
-import Navbars from "views/Documentation/Sections/Navbars.jsx";
-import Tables from "views/Documentation/Sections/Tables.jsx";
-import Typography from "views/Documentation/Sections/Typography.jsx";
+// import Alerts from "views/Documentation/Sections/Alerts.jsx";
+// import Badges from "views/Documentation/Sections/Badges.jsx";
+// import Breadcrumbs from "views/Documentation/Sections/Breadcrumbs.jsx";
+// import Buttons from "views/Documentation/Sections/Buttons.jsx";
+// import Cards from "views/Documentation/Sections/Cards.jsx";
+// import Dropdowns from "views/Documentation/Sections/Dropdowns.jsx";
+// import Forms from "views/Documentation/Sections/Forms.jsx";
+// import Modals from "views/Documentation/Sections/Modals.jsx";
+// import Navs from "views/Documentation/Sections/Navs.jsx";
+// import Navbars from "views/Documentation/Sections/Navbars.jsx";
+// import Tables from "views/Documentation/Sections/Tables.jsx";
+// import Typography from "views/Documentation/Sections/Typography.jsx";
 // Plugins
-import Charts from "views/Documentation/Sections/Charts.jsx";
-import Icons from "views/Documentation/Sections/Icons.jsx";
-import PerfectScrollbar from "views/Documentation/Sections/PerfectScrollbar.jsx";
+// import Charts from "views/Documentation/Sections/Charts.jsx";
+// import Icons from "views/Documentation/Sections/Icons.jsx";
+// import PerfectScrollbar from "views/Documentation/Sections/PerfectScrollbar.jsx";
 
 var docsRoutes = [
   {
@@ -73,112 +78,138 @@ var docsRoutes = [
     ]
   },
   {
-    name: "Core Components",
-    path: "/documentation/custom-upload",
+    name: "Foundation",
+    path: "/documentation/colors",
     routes: [
       {
-        path: "/documentation/fixed-plugin",
-        component: FixedPlugin,
-        name: "Fixed Plugin"
+        path: "/documentation/colors",
+        component: Colors,
+        name: "Colors"
       },
       {
-        path: "/documentation/footer",
-        component: Footer,
-        name: "Footer"
-      },
-      {
-        path: "/documentation/navbar",
-        component: Navbar,
-        name: "App Navbar"
-      },
-      {
-        path: "/documentation/sidebar",
-        component: Sidebar,
-        name: "Sidebar"
-      }
-    ]
-  },
-  {
-    name: "Reactstrap Components",
-    path: "/documentation/alert",
-    routes: [
-      {
-        path: "/documentation/alert",
-        component: Alerts,
-        name: "Alerts"
-      },
-      {
-        path: "/documentation/badge",
-        component: Badges,
-        name: "Badges"
-      },
-      {
-        path: "/documentation/breadcrumbs",
-        component: Breadcrumbs,
-        name: "Breadcrumbs"
-      },
-      {
-        path: "/documentation/buttons",
-        component: Buttons,
-        name: "Buttons"
-      },
-      {
-        path: "/documentation/cards",
-        component: Cards,
-        name: "Cards"
-      },
-      {
-        path: "/documentation/dropdowns",
-        component: Dropdowns,
-        name: "Dropdowns"
-      },
-      {
-        path: "/documentation/forms",
-        component: Forms,
-        name: "Forms"
-      },
-      {
-        path: "/documentation/modal",
-        component: Modals,
-        name: "Modals"
-      },
-      {
-        path: "/documentation/navbars",
-        component: Navbars,
-        name: "Navbars"
-      },
-      {
-        path: "/documentation/navs",
-        component: Navs,
-        name: "Navs"
-      },
-      {
-        path: "/documentation/tables",
-        component: Tables,
-        name: "Tables"
+        path: "/documentation/grid",
+        component: Grid,
+        name: "Grid"
       },
       {
         path: "/documentation/typography",
         component: Typography,
         name: "Typography"
-      }
+      },
+      {
+        path: "/documentation/icons",
+        component: Icons,
+        name: "Icons"
+      },
+    ]
+  },
+  {
+    name: "Core Components",
+    path: "/documentation/custom-upload",
+    routes: [
+      // {
+      //   path: "/documentation/fixed-plugin",
+      //   component: FixedPlugin,
+      //   name: "Fixed Plugin"
+      // },
+      // {
+      //   path: "/documentation/footer",
+      //   component: Footer,
+      //   name: "Footer"
+      // },
+      // {
+      //   path: "/documentation/navbar",
+      //   component: Navbar,
+      //   name: "App Navbar"
+      // },
+      // {
+      //   path: "/documentation/sidebar",
+      //   component: Sidebar,
+      //   name: "Sidebar"
+      // }
+    ]
+  },
+  {
+    name: "Restyled Components",
+    path: "/documentation/alert",
+    routes: [
+      // {
+      //   path: "/documentation/alert",
+      //   component: Alerts,
+      //   name: "Alerts"
+      // },
+      // {
+      //   path: "/documentation/badge",
+      //   component: Badges,
+      //   name: "Badges"
+      // },
+      // {
+      //   path: "/documentation/breadcrumbs",
+      //   component: Breadcrumbs,
+      //   name: "Breadcrumbs"
+      // },
+      // {
+      //   path: "/documentation/buttons",
+      //   component: Buttons,
+      //   name: "Buttons"
+      // },
+      // {
+      //   path: "/documentation/cards",
+      //   component: Cards,
+      //   name: "Cards"
+      // },
+      // {
+      //   path: "/documentation/dropdowns",
+      //   component: Dropdowns,
+      //   name: "Dropdowns"
+      // },
+      // {
+      //   path: "/documentation/forms",
+      //   component: Forms,
+      //   name: "Forms"
+      // },
+      // {
+      //   path: "/documentation/modal",
+      //   component: Modals,
+      //   name: "Modals"
+      // },
+      // {
+      //   path: "/documentation/navbars",
+      //   component: Navbars,
+      //   name: "Navbars"
+      // },
+      // {
+      //   path: "/documentation/navs",
+      //   component: Navs,
+      //   name: "Navs"
+      // },
+      // {
+      //   path: "/documentation/tables",
+      //   component: Tables,
+      //   name: "Tables"
+      // },
+      // {
+      //   path: "/documentation/typography",
+      //   component: Typography,
+      //   name: "Typography"
+      // }
     ]
   },
   {
     name: "Plugins",
     path: "/documentation/charts",
     routes: [
-      { path: "/documentation/charts", component: Charts, name: "Charts" },
-      {
-        path: "/documentation/icons",
-        component: Icons,
-        name: "Nucleo Icons"
-      },
-      {
-        path: "/documentation/perfect-scrollbar",
-        component: PerfectScrollbar,
-        name: "PerfectScrollbar"
-      }
+      // { path: "/documentation/charts", component: Charts, name: "Charts" },
+      // {
+      //   path: "/documentation/icons",
+      //   component: Icons,
+      //   name: "Nucleo Icons"
+      // },
+      // {
+      //   path: "/documentation/perfect-scrollbar",
+      //   component: PerfectScrollbar,
+      //   name: "PerfectScrollbar"
+      // }
     ]
   },
   { redirect: true, path: "/documentation", pathTo: "/documentation/overview" }
