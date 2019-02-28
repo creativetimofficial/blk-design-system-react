4f6ed2877ba5f226673fa84a1543165f2ce4e1f4
Manu <emanuelnazare@yahoo.ro>
2019-02-28 17:22:37 +0200

    - Small bus on mobile - TODO: README

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -16,8 +16,8 @@ class PageHeader extends React.Component {
         <div className="squares square7" />
         <Container>
           <div className="content-center brand">
-            <h1 className="h1-seo">BLK•React</h1>
-            <h3>
+            <h1 className="h1-seo">BLK• React</h1>
+            <h3 className="d-none d-sm-block">
               A beautiful Design System for Bootstrap 4 (reactstrap) and React.
               It's Free and Open Source.
             </h3>
