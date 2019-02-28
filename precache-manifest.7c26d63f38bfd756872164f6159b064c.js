0890168a9f3388e5ebe80dc498947f7f14d8187e
Manu <emanuelnazare@yahoo.ro>
2019-02-28 16:15:22 +0200

    Updates

--- precache-manifest.7c26d63f38bfd756872164f6159b064c.js
+++ precache-manifest.7c26d63f38bfd756872164f6159b064c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "ca25707397fcc982d7dc1f82555f09a0",
-    "url": "/blk-design-system-react/static/media/square1.ca257073.png"
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/blk-design-system-react/static/media/square5.ff004a0e.png"
   },
   {
-    "revision": "9d1f0159c5923a9ea4a5",
+    "revision": "c7aae2fe659f616d9d9f",
     "url": "/blk-design-system-react/static/css/main.982114d8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = [
     "url": "/blk-design-system-react/static/js/runtime~main.2b185c2d.js"
   },
   {
-    "revision": "74ae67d18a70c2d02860",
-    "url": "/blk-design-system-react/static/js/2.74ae67d1.chunk.js"
+    "revision": "72c1f972b74d25cf9def",
+    "url": "/blk-design-system-react/static/js/2.72c1f972.chunk.js"
   },
   {
     "revision": "e37326c7e0fd9826c1a35b231340f58b",
@@ -60,44 +60,44 @@ self.__precacheManifest = [
     "url": "/blk-design-system-react/static/media/etherum.4fd59ead.png"
   },
   {
-    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
-    "url": "/blk-design-system-react/static/media/nucleo.f0b489a5.woff"
-  },
-  {
     "revision": "5987dd12fea78ce5f97ae601b08ec03c",
     "url": "/blk-design-system-react/static/media/nucleo.5987dd12.woff2"
   },
   {
-    "revision": "03ef1918e505c3e3471f9369ef7a638f",
-    "url": "/blk-design-system-react/static/media/nucleo.03ef1918.eot"
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/blk-design-system-react/static/media/nucleo.f0b489a5.woff"
   },
   {
     "revision": "b17a118e13e53558658b681a0ebdad82",
     "url": "/blk-design-system-react/static/media/nucleo.b17a118e.ttf"
   },
   {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/blk-design-system-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
     "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
     "url": "/blk-design-system-react/static/media/dots.b81dabc0.png"
   },
   {
-    "revision": "9d1f0159c5923a9ea4a5",
-    "url": "/blk-design-system-react/static/js/main.9d1f0159.chunk.js"
+    "revision": "c7aae2fe659f616d9d9f",
+    "url": "/blk-design-system-react/static/js/main.c7aae2fe.chunk.js"
   },
   {
     "revision": "6619b9792d7d108bb589b51bbd560278",
     "url": "/blk-design-system-react/static/media/square2.6619b979.png"
   },
   {
-    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
-    "url": "/blk-design-system-react/static/media/square5.ff004a0e.png"
-  },
-  {
-    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
-    "url": "/blk-design-system-react/static/media/square4.8fdf9ee2.png"
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/blk-design-system-react/static/media/square1.ca257073.png"
   },
   {
     "revision": "23fa76237f0066f4ffd8f20e77449887",
     "url": "/blk-design-system-react/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/blk-design-system-react/static/media/square4.8fdf9ee2.png"
   },
   {
     "revision": "9d4a558e7169ed763a05715627bc088c",
@@ -156,7 +156,7 @@ self.__precacheManifest = [
     "url": "/blk-design-system-react/static/media/ripp.98742a82.png"
   },
   {
-    "revision": "b8d798ea32e2c67aa4d5fb58c3ba2097",
+    "revision": "13a93c14fb079c02a1271e7f8342a3cf",
     "url": "/blk-design-system-react/index.html"
   }
 ];
