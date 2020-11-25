5be0e15bb0848841a2d1d8ac9afcc4ca13fef419
EINazare <emanuelioannazare@gmail.com>
2020-11-25 18:26:56 +0200

    refactor: change class components to function ones
    This will support hooks.

--- src/views/IndexSections/Notifications.js
+++ src/views/IndexSections/Notifications.js
@@ -19,52 +19,48 @@ import React from "react";
 // reactstrap components
 import { UncontrolledAlert, Container } from "reactstrap";
 
-class Notifications extends React.Component {
-  render() {
-    return (
-      <div className="section section-notifications" id="notifications">
-        <Container>
-          <div className="space" />
-          <h3>Notifications</h3>
-          <UncontrolledAlert className="alert-with-icon" color="primary">
-            <span data-notify="icon" className="tim-icons icon-coins" />
-            <span>
-              <b>Congrats! -</b>
-              This is a regular notification made with ".alert-primary"
-            </span>
-          </UncontrolledAlert>
-          <UncontrolledAlert className="alert-with-icon" color="info">
-            <span data-notify="icon" className="tim-icons icon-trophy" />
-            <span>
-              <b>Heads up! -</b>
-              This is a regular notification made with ".alert-info"
-            </span>
-          </UncontrolledAlert>
-          <UncontrolledAlert className="alert-with-icon" color="success">
-            <span data-notify="icon" className="tim-icons icon-bell-55" />
-            <span>
-              <b>Well done! -</b>
-              This is a regular notification made with ".alert-success"
-            </span>
-          </UncontrolledAlert>
-          <UncontrolledAlert className="alert-with-icon" color="warning">
-            <span data-notify="icon" className="tim-icons icon-bulb-63" />
-            <span>
-              <b>Warning! -</b>
-              This is a regular notification made with ".alert-warning"
-            </span>
-          </UncontrolledAlert>
-          <UncontrolledAlert className="alert-with-icon" color="danger">
-            <span data-notify="icon" className="tim-icons icon-support-17" />
-            <span>
-              <b>Oh snap! -</b>
-              This is a regular notification made with ".alert-danger"
-            </span>
-          </UncontrolledAlert>
-        </Container>
-      </div>
-    );
-  }
+export default function Notifications() {
+  return (
+    <div className="section section-notifications" id="notifications">
+      <Container>
+        <div className="space" />
+        <h3>Notifications</h3>
+        <UncontrolledAlert className="alert-with-icon" color="primary">
+          <span data-notify="icon" className="tim-icons icon-coins" />
+          <span>
+            <b>Congrats! -</b>
+            This is a regular notification made with ".alert-primary"
+          </span>
+        </UncontrolledAlert>
+        <UncontrolledAlert className="alert-with-icon" color="info">
+          <span data-notify="icon" className="tim-icons icon-trophy" />
+          <span>
+            <b>Heads up! -</b>
+            This is a regular notification made with ".alert-info"
+          </span>
+        </UncontrolledAlert>
+        <UncontrolledAlert className="alert-with-icon" color="success">
+          <span data-notify="icon" className="tim-icons icon-bell-55" />
+          <span>
+            <b>Well done! -</b>
+            This is a regular notification made with ".alert-success"
+          </span>
+        </UncontrolledAlert>
+        <UncontrolledAlert className="alert-with-icon" color="warning">
+          <span data-notify="icon" className="tim-icons icon-bulb-63" />
+          <span>
+            <b>Warning! -</b>
+            This is a regular notification made with ".alert-warning"
+          </span>
+        </UncontrolledAlert>
+        <UncontrolledAlert className="alert-with-icon" color="danger">
+          <span data-notify="icon" className="tim-icons icon-support-17" />
+          <span>
+            <b>Oh snap! -</b>
+            This is a regular notification made with ".alert-danger"
+          </span>
+        </UncontrolledAlert>
+      </Container>
+    </div>
+  );
 }
-
-export default Notifications;
