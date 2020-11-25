d89e2bf5d6506b84b50b0735afd6cf0a47c17eb4
EINazare <emanuelioannazare@gmail.com>
2020-11-25 18:26:47 +0200

    refactor: change class components to function ones
    This will support hooks.

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -20,29 +20,25 @@ import React from "react";
 // reactstrap components
 import { Container } from "reactstrap";
 
-class PageHeader extends React.Component {
-  render() {
-    return (
-      <div className="page-header header-filter">
-        <div className="squares square1" />
-        <div className="squares square2" />
-        <div className="squares square3" />
-        <div className="squares square4" />
-        <div className="squares square5" />
-        <div className="squares square6" />
-        <div className="squares square7" />
-        <Container>
-          <div className="content-center brand">
-            <h1 className="h1-seo">BLK• React</h1>
-            <h3 className="d-none d-sm-block">
-              A beautiful Design System for Bootstrap 4 (reactstrap) and React.
-              It's Free and Open Source.
-            </h3>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+export default function PageHeader() {
+  return (
+    <div className="page-header header-filter">
+      <div className="squares square1" />
+      <div className="squares square2" />
+      <div className="squares square3" />
+      <div className="squares square4" />
+      <div className="squares square5" />
+      <div className="squares square6" />
+      <div className="squares square7" />
+      <Container>
+        <div className="content-center brand">
+          <h1 className="h1-seo">BLK• React</h1>
+          <h3 className="d-none d-sm-block">
+            A beautiful Design System for Bootstrap 4 (reactstrap) and React.
+            It's Free and Open Source.
+          </h3>
+        </div>
+      </Container>
+    </div>
+  );
 }
-
-export default PageHeader;
