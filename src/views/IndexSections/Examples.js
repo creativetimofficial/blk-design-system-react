81f4b46093812a7cc35a12ed5b3c38a11a68e5cb
EINazare <emanuelioannazare@gmail.com>
2020-11-25 18:26:51 +0200

    refactor: change class components to function ones
    This will support hooks.

--- src/views/IndexSections/Examples.js
+++ src/views/IndexSections/Examples.js
@@ -20,57 +20,53 @@ import { Link } from "react-router-dom";
 // reactstrap components
 import { Button, Container, Row, Col } from "reactstrap";
 
-class Examples extends React.Component {
-  render() {
-    return (
-      <div className="section section-examples" data-background-color="black">
-        <img
-          alt="..."
-          className="path"
-          src={require("assets/img/path1.png").default}
-        />
-        <div className="space-50" />
-        <Container className="text-center">
-          <Row>
-            <Col sm="6">
-              <Link to="landing-page">
-                <img
-                  alt="..."
-                  className="img-raised"
-                  src={require("assets/img/landing-page.png").default}
-                />
-              </Link>
-              <Button
-                className="btn-simple btn-round"
-                color="primary"
-                to="landing-page"
-                tag={Link}
-              >
-                View Landing Page
-              </Button>
-            </Col>
-            <Col sm="6">
-              <Link to="profile-page">
-                <img
-                  alt="..."
-                  className="img-raised"
-                  src={require("assets/img/profile-page.png").default}
-                />
-              </Link>
-              <Button
-                className="btn-simple btn-round"
-                color="primary"
-                to="profile-page"
-                tag={Link}
-              >
-                View Profile Page
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+export default function Examples() {
+  return (
+    <div className="section section-examples" data-background-color="black">
+      <img
+        alt="..."
+        className="path"
+        src={require("assets/img/path1.png").default}
+      />
+      <div className="space-50" />
+      <Container className="text-center">
+        <Row>
+          <Col sm="6">
+            <Link to="landing-page">
+              <img
+                alt="..."
+                className="img-raised"
+                src={require("assets/img/landing-page.png").default}
+              />
+            </Link>
+            <Button
+              className="btn-simple btn-round"
+              color="primary"
+              to="landing-page"
+              tag={Link}
+            >
+              View Landing Page
+            </Button>
+          </Col>
+          <Col sm="6">
+            <Link to="profile-page">
+              <img
+                alt="..."
+                className="img-raised"
+                src={require("assets/img/profile-page.png").default}
+              />
+            </Link>
+            <Button
+              className="btn-simple btn-round"
+              color="primary"
+              to="profile-page"
+              tag={Link}
+            >
+              View Profile Page
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
-
-export default Examples;
