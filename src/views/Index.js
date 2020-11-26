bc498d18e27aaf9448f07944045cc0b2ea50e089
EINazare <emanuelioannazare@gmail.com>
2020-11-26 11:34:05 +0200

    refactor: change class to function component

--- src/views/Index.js
+++ src/views/Index.js
@@ -35,37 +35,34 @@ import Signup from "views/IndexSections/Signup.js";
 import Examples from "views/IndexSections/Examples.js";
 import Download from "views/IndexSections/Download.js";
 
-class Index extends React.Component {
-  componentDidMount() {
+export default function Index() {
+  React.useEffect(() => {
     document.body.classList.toggle("index-page");
-  }
-  componentWillUnmount() {
-    document.body.classList.toggle("index-page");
-  }
-  render() {
-    return (
-      <>
-        <IndexNavbar />
-        <div className="wrapper">
-          <PageHeader />
-          <div className="main">
-            <Basics />
-            <Navbars />
-            <Tabs />
-            <Pagination />
-            <Notifications />
-            <Typography />
-            <JavaScript />
-            <NucleoIcons />
-            <Signup />
-            <Examples />
-            <Download />
-          </div>
-          <Footer />
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("index-page");
+    };
+  });
+  return (
+    <>
+      <IndexNavbar />
+      <div className="wrapper">
+        <PageHeader />
+        <div className="main">
+          <Basics />
+          <Navbars />
+          <Tabs />
+          <Pagination />
+          <Notifications />
+          <Typography />
+          <JavaScript />
+          <NucleoIcons />
+          <Signup />
+          <Examples />
+          <Download />
         </div>
-      </>
-    );
-  }
+        <Footer />
+      </div>
+    </>
+  );
 }
-
-export default Index;
