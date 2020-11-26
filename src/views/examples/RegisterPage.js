c412d4acdf0b31cff8eee768cf024a44e39a2e78
EINazare <emanuelioannazare@gmail.com>
2020-11-26 11:53:19 +0200

    refactor: change class to function component

--- src/views/examples/RegisterPage.js
+++ src/views/examples/RegisterPage.js
@@ -42,198 +42,179 @@ import {
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import Footer from "components/Footer/Footer.js";
 
-class RegisterPage extends React.Component {
-  state = {
-    squares1to6: "",
-    squares7and8: "",
-  };
-  componentDidMount() {
+export default function RegisterPage() {
+  const [squares1to6, setSquares1to6] = React.useState("");
+  const [squares7and8, setSquares7and8] = React.useState("");
+  const [fullNameFocus, setFullNameFocus] = React.useState(false);
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  const [passwordFocus, setPasswordFocus] = React.useState(false);
+  React.useEffect(() => {
     document.body.classList.toggle("register-page");
-    document.documentElement.addEventListener("mousemove", this.followCursor);
-  }
-  componentWillUnmount() {
-    document.body.classList.toggle("register-page");
-    document.documentElement.removeEventListener(
-      "mousemove",
-      this.followCursor
-    );
-  }
-  followCursor = (event) => {
+    document.documentElement.addEventListener("mousemove", followCursor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.removeEventListener("mousemove", followCursor);
+    };
+  });
+  const followCursor = (event) => {
     let posX = event.clientX - window.innerWidth / 2;
     let posY = event.clientY - window.innerWidth / 6;
-    this.setState({
-      squares1to6:
-        "perspective(500px) rotateY(" +
+    setSquares1to6(
+      "perspective(500px) rotateY(" +
         posX * 0.05 +
         "deg) rotateX(" +
         posY * -0.05 +
-        "deg)",
-      squares7and8:
-        "perspective(500px) rotateY(" +
+        "deg)"
+    );
+    setSquares7and8(
+      "perspective(500px) rotateY(" +
         posX * 0.02 +
         "deg) rotateX(" +
         posY * -0.02 +
-        "deg)",
-    });
-  };
-  render() {
-    return (
-      <>
-        <ExamplesNavbar />
-        <div className="wrapper">
-          <div className="page-header">
-            <div className="page-header-image" />
-            <div className="content">
-              <Container>
-                <Row>
-                  <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
-                    <div
-                      className="square square-7"
-                      id="square7"
-                      style={{ transform: this.state.squares7and8 }}
-                    />
-                    <div
-                      className="square square-8"
-                      id="square8"
-                      style={{ transform: this.state.squares7and8 }}
-                    />
-                    <Card className="card-register">
-                      <CardHeader>
-                        <CardImg
-                          alt="..."
-                          src={
-                            require("assets/img/square-purple-1.png").default
-                          }
-                        />
-                        <CardTitle tag="h4">Register</CardTitle>
-                      </CardHeader>
-                      <CardBody>
-                        <Form className="form">
-                          <InputGroup
-                            className={classnames({
-                              "input-group-focus": this.state.fullNameFocus,
-                            })}
-                          >
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="tim-icons icon-single-02" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Full Name"
-                              type="text"
-                              onFocus={(e) =>
-                                this.setState({ fullNameFocus: true })
-                              }
-                              onBlur={(e) =>
-                                this.setState({ fullNameFocus: false })
-                              }
-                            />
-                          </InputGroup>
-                          <InputGroup
-                            className={classnames({
-                              "input-group-focus": this.state.emailFocus,
-                            })}
-                          >
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="tim-icons icon-email-85" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Email"
-                              type="text"
-                              onFocus={(e) =>
-                                this.setState({ emailFocus: true })
-                              }
-                              onBlur={(e) =>
-                                this.setState({ emailFocus: false })
-                              }
-                            />
-                          </InputGroup>
-                          <InputGroup
-                            className={classnames({
-                              "input-group-focus": this.state.passwordFocus,
-                            })}
-                          >
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="tim-icons icon-lock-circle" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Password"
-                              type="text"
-                              onFocus={(e) =>
-                                this.setState({ passwordFocus: true })
-                              }
-                              onBlur={(e) =>
-                                this.setState({ passwordFocus: false })
-                              }
-                            />
-                          </InputGroup>
-                          <FormGroup check className="text-left">
-                            <Label check>
-                              <Input type="checkbox" />
-                              <span className="form-check-sign" />I agree to the{" "}
-                              <a
-                                href="#pablo"
-                                onClick={(e) => e.preventDefault()}
-                              >
-                                terms and conditions
-                              </a>
-                              .
-                            </Label>
-                          </FormGroup>
-                        </Form>
-                      </CardBody>
-                      <CardFooter>
-                        <Button className="btn-round" color="primary" size="lg">
-                          Get Started
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  </Col>
-                </Row>
-                <div className="register-bg" />
-                <div
-                  className="square square-1"
-                  id="square1"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-2"
-                  id="square2"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-3"
-                  id="square3"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-4"
-                  id="square4"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-5"
-                  id="square5"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-                <div
-                  className="square square-6"
-                  id="square6"
-                  style={{ transform: this.state.squares1to6 }}
-                />
-              </Container>
-            </div>
-          </div>
-          <Footer />
-        </div>
-      </>
+        "deg)"
     );
-  }
+  };
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <div className="page-header-image" />
+          <div className="content">
+            <Container>
+              <Row>
+                <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
+                  <div
+                    className="square square-7"
+                    id="square7"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <div
+                    className="square square-8"
+                    id="square8"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <Card className="card-register">
+                    <CardHeader>
+                      <CardImg
+                        alt="..."
+                        src={require("assets/img/square-purple-1.png").default}
+                      />
+                      <CardTitle tag="h4">Register</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                      <Form className="form">
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": fullNameFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-single-02" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Full Name"
+                            type="text"
+                            onFocus={(e) => setFullNameFocus(true)}
+                            onBlur={(e) => setFullNameFocus(false)}
+                          />
+                        </InputGroup>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": emailFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-email-85" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Email"
+                            type="text"
+                            onFocus={(e) => setEmailFocus(true)}
+                            onBlur={(e) => setEmailFocus(false)}
+                          />
+                        </InputGroup>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": passwordFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-lock-circle" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Password"
+                            type="text"
+                            onFocus={(e) => setPasswordFocus(true)}
+                            onBlur={(e) => setPasswordFocus(false)}
+                          />
+                        </InputGroup>
+                        <FormGroup check className="text-left">
+                          <Label check>
+                            <Input type="checkbox" />
+                            <span className="form-check-sign" />I agree to the{" "}
+                            <a
+                              href="#pablo"
+                              onClick={(e) => e.preventDefault()}
+                            >
+                              terms and conditions
+                            </a>
+                            .
+                          </Label>
+                        </FormGroup>
+                      </Form>
+                    </CardBody>
+                    <CardFooter>
+                      <Button className="btn-round" color="primary" size="lg">
+                        Get Started
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              </Row>
+              <div className="register-bg" />
+              <div
+                className="square square-1"
+                id="square1"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-2"
+                id="square2"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-3"
+                id="square3"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-4"
+                id="square4"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-5"
+                id="square5"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-6"
+                id="square6"
+                style={{ transform: squares1to6 }}
+              />
+            </Container>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
-
-export default RegisterPage;
