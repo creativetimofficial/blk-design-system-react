8c4c1250013662392bc873f59a8874b435c8a4ca
EINazare <emanuelioannazare@gmail.com>
2020-12-08 16:17:23 +0200

    fix: solve warnings

--- src/views/Documentation/Sections/restyled-components/Switch.js
+++ src/views/Documentation/Sections/restyled-components/Switch.js
@@ -18,7 +18,7 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
-import { Col, CardSubtitle, Alert, CustomInput } from "reactstrap";
+import { Col, CustomInput } from "reactstrap";
 
 const codeSwitchExample = `import React from "react";
 
